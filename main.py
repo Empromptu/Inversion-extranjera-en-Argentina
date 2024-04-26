7c6599580d382ea55692896ca8466774dc328319
Guido Pauletti <guido13pauletti@gmail.com>
2024-04-26 19:51:53 -0300

    agregar archivos

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Trabajo Practico 1, Laboratorio de datos 1C 2024, Grupo 3
+
+# INTEGRANTES:
+#   Pauletti, Guido
+#   Paredes, Gerson
+#   Catania, Juan Ignacio
+
+# Como primera medida importamos todas las herramientas necesarias.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from inline_sql import sql, sql_val
+
